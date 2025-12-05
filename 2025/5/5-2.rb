65860d3e5867038079b3b4c60786ab5733cf13dd
twoo <tamsin@estradiol.cloud>
2025-12-05 10:51:38 -0800

    day 5; refactor

--- 2025/5/5-2.rb
+++ 2025/5/5-2.rb
@@ -1,34 +1,28 @@
 #!/bin/ruby
 
-fresh_ranges = []
-
-File.readlines("5.input").each do |l|
-  break if l.strip.empty?
-  first, last = l.split('-').map(&:to_i)
-  fresh_ranges << (first..last)
+def merge_range(r, s)
+  ([r.min, s.min].min..[r.max, s.max].max)
 end
 
-def consolidate_ranges(fresh_ranges)
-  old_count = fresh_ranges.count
-  
-  fresh_ranges.sort! do |a, b|
-    if a.overlap?(b)
-      0
-    elsif a.first < b.first
-      -1
-    else
-      1
+def compact_ranges(new_range, ranges)
+  result = nil
+
+  ranges.each do |range|
+    if range.overlap?(new_range)
+      ranges.delete(range)
+      result = compact_ranges(merge_range(range, new_range), ranges)
     end
   end
 
-  new_ranges = fresh_ranges.slice_when { |a, b| !a.overlap?(b) }.map do |ranges|
-    min = ranges.map(&:min).min
-    max = ranges.map(&:max).max
-    (min..max)
-  end
-
-  return new_ranges if new_ranges.count == old_count
-  consolidate_ranges(new_ranges)
+  result || (ranges << new_range)
 end
 
-puts consolidate_ranges(fresh_ranges).map(&:size).reduce(:+)
+fresh_ranges = []
+
+File.readlines("5.input").each do |l|
+  break if l.strip.empty? # ignore after first blank line
+  first, last = l.split('-').map(&:to_i)
+  fresh_ranges = compact_ranges((first..last), fresh_ranges)
+end
+
+puts fresh_ranges.map(&:size).reduce(:+)
